3dd353d36850d5a0ea0d9828cf0ad4313b3a4360
史建忠 <shijianzhongg@icloud.com>
2025-04-11 09:13:02 +0800

    换装图片

--- src/renderer/src/components/live2d/index.vue
+++ src/renderer/src/components/live2d/index.vue
@@ -100,202 +100,202 @@
     {
       name: '拉菲',
       path: 'models/lafei/lafei.model3.json',
-      thumbnail: 'models/lafei/thumbnail.png'
+      thumbnail: 'models/lafei/textures/texture_00.png'
     },
     {
       name: '爱尔德里奇5',
       path: 'models/aierdeliqi_5/aierdeliqi_5.model3.json',
-      thumbnail: 'models/aierdeliqi_5/thumbnail.png'
+      thumbnail: 'models/aierdeliqi_5/textures/texture_00.png'
     },
     {
       name: '爱尔德里奇4',
       path: 'models/aierdeliqi_4/aierdeliqi_4.model3.json',
-      thumbnail: 'models/aierdeliqi_4/thumbnail.png'
+      thumbnail: 'models/aierdeliqi_4/textures/texture_00.png'
     },
     {
       name: '爱丹2',
       path: 'models/aidang_2/aidang_2.model3.json',
-      thumbnail: 'models/aidang_2/thumbnail.png'
+      thumbnail: 'models/aidang_2/textures/texture_00.png'
     },
     {
       name: 'bisimai_2',
       path: 'models/bisimai_2/bisimai_2.model3.json',
-      thumbnail: 'models/bisimai_2/thumbnail.png'
+      thumbnail: 'models/bisimai_2/textures/texture_00.png'
     },
     {
       name: '扎拉2',
       path: 'models/zhala_2/zhala_2.model3.json',
-      thumbnail: 'models/zhala_2/thumbnail.png'
+      thumbnail: 'models/zhala_2/textures/texture_00.png'
     },
     {
       name: 'Z46',
       path: 'models/z46_2/z46_2.model3.json',
-      thumbnail: 'models/z46_2/thumbnail.png'
+      thumbnail: 'models/z46_2/textures/texture_00.png'
     },
     {
       name: 'Z23',
       path: 'models/z23/z23.model3.json',
-      thumbnail: 'models/z23/thumbnail.png'
+      thumbnail: 'models/z23/textures/texture_00.png'
     },
     {
       name: '伊吹2',
       path: 'models/yichui_2/yichui_2.model3.json',
-      thumbnail: 'models/yichui_2/thumbnail.png'
+      thumbnail: 'models/yichui_2/textures/texture_00.png'
     },
     {
       name: '雪风',
       path: 'models/xuefeng/xuefeng.model3.json',
-      thumbnail: 'models/xuefeng/thumbnail.png'
+      thumbnail: 'models/xuefeng/textures/texture_00.png'
     },
     {
       name: '吸血鬼4',
       path: 'models/xixuegui_4/xixuegui_4.model3.json',
-      thumbnail: 'models/xixuegui_4/thumbnail.png'
+      thumbnail: 'models/xixuegui_4/textures/texture_00.png'
     },
     {
       name: '翔鹤2',
       path: 'models/xianghe_2/xianghe_2.model3.json',
-      thumbnail: 'models/xianghe_2/thumbnail.png'
+      thumbnail: 'models/xianghe_2/textures/texture_00.png'
     },
     {
       name: '提尔比茨2',
       path: 'models/tierbici_2/tierbici_2.model3.json',
-      thumbnail: 'models/tierbici_2/thumbnail.png'
+      thumbnail: 'models/tierbici_2/textures/texture_00.png'
     },
     {
       name: '天狼星3',
       path: 'models/tianlangxing_3/tianlangxing_3.model3.json',
-      thumbnail: 'models/tianlangxing_3/thumbnail.png'
+      thumbnail: 'models/tianlangxing_3/textures/texture_00.png'
     },
     {
       name: '太原2',
       path: 'models/taiyuan_2/taiyuan_2.model3.json',
-      thumbnail: 'models/taiyuan_2/thumbnail.png'
+      thumbnail: 'models/taiyuan_2/textures/texture_00.png'
     },
     {
       name: '斯佩伯爵5',
       path: 'models/sipeibojue_5/sipeibojue_5.model3.json',
-      thumbnail: 'models/sipeibojue_5/thumbnail.png'
+      thumbnail: 'models/sipeibojue_5/textures/texture_00.png'
     },
     {
       name: '圣路易斯3',
       path: 'models/shengluyisi_3/shengluyisi_3.model3.json',
-      thumbnail: 'models/shengluyisi_3/thumbnail.png'
+      thumbnail: 'models/shengluyisi_3/textures/texture_00.png'
     },
     {
       name: '圣路易斯2',
       path: 'models/shengluyisi_2/shengluyisi_2.model3.json',
-      thumbnail: 'models/shengluyisi_2/thumbnail.png'
+      thumbnail: 'models/shengluyisi_2/textures/texture_00.png'
     },
     {
       name: '齐柏林2',
       path: 'models/qibolin_2/qibolin_2.model3.json',
-      thumbnail: 'models/qibolin_2/thumbnail.png'
+      thumbnail: 'models/qibolin_2/textures/texture_00.png'
     },
     {
       name: '平海4',
       path: 'models/pinghai_4/pinghai_4.model3.json',
-      thumbnail: 'models/pinghai_4/thumbnail.png'
+      thumbnail: 'models/pinghai_4/textures/texture_00.png'
     },
     {
       name: '宁海4',
       path: 'models/ninghai_4/ninghai_4.model3.json',
-      thumbnail: 'models/ninghai_4/thumbnail.png'
+      thumbnail: 'models/ninghai_4/textures/texture_00.png'
     },
     {
       name: '明石',
       path: 'models/mingshi/mingshi.model3.json',
-      thumbnail: 'models/mingshi/thumbnail.png'
+      thumbnail: 'models/mingshi/textures/texture_00.png'
     },
     {
       name: '凌波',
       path: 'models/lingbo/lingbo.model3.json',
-      thumbnail: 'models/lingbo/thumbnail.png'
+      thumbnail: 'models/lingbo/textures/texture_00.png'
     },
     {
       name: '拉菲4',
       path: 'models/lafei_4/lafei_4.model3.json',
-      thumbnail: 'models/lafei_4/thumbnail.png'
+      thumbnail: 'models/lafei_4/textures/texture_00.png'
     },
     {
       name: '克利夫兰3',
       path: 'models/kelifulan_3/kelifulan_3.model3.json',
-      thumbnail: 'models/kelifulan_3/thumbnail.png'
+      thumbnail: 'models/kelifulan_3/textures/texture_00.png'
     },
     {
       name: '火奴鲁鲁5',
       path: 'models/huonululu_5/huonululu_5.model3.json',
-      thumbnail: 'models/huonululu_5/thumbnail.png'
+      thumbnail: 'models/huonululu_5/textures/texture_00.png'
     },
     {
       name: '火奴鲁鲁3',
       path: 'models/huonululu_3/huonululu_3.model3.json',
-      thumbnail: 'models/huonululu_3/thumbnail.png'
+      thumbnail: 'models/huonululu_3/textures/texture_00.png'
     },
     {
       name: '黄金方舟3',
       path: 'models/huangjiafangzhou_3/huangjiafangzhou_3.model3.json',
-      thumbnail: 'models/huangjiafangzhou_3/thumbnail.png'
+      thumbnail: 'models/huangjiafangzhou_3/textures/texture_00.png'
     },
     {
       name: '黑太子2',
       path: 'models/heitaizi_2/heitaizi_2.model3.json',
-      thumbnail: 'models/heitaizi_2/thumbnail.png'
+      thumbnail: 'models/heitaizi_2/textures/texture_00.png'
     },
     {
       name: '根艾森瑙2',
       path: 'models/genaisennao_2/genaisennao_2.model3.json',
-      thumbnail: 'models/genaisennao_2/thumbnail.png'
+      thumbnail: 'models/genaisennao_2/textures/texture_00.png'
     },
     {
       name: '敦刻尔克2',
       path: 'models/dunkeerke_2/dunkeerke_2.model3.json',
-      thumbnail: 'models/dunkeerke_2/thumbnail.png'
+      thumbnail: 'models/dunkeerke_2/textures/texture_00.png'
     },
     {
       name: '独角兽4',
       path: 'models/dujiaoshou_4/dujiaoshou_4.model3.json',
-      thumbnail: 'models/dujiaoshou_4/thumbnail.png'
+      thumbnail: 'models/dujiaoshou_4/textures/texture_00.png'
     },
     {
       name: '德意志3',
       path: 'models/deyizhi_3/deyizhi_3.model3.json',
-      thumbnail: 'models/deyizhi_3/thumbnail.png'
+      thumbnail: 'models/deyizhi_3/textures/texture_00.png'
     },
     {
       name: '大凤2',
       path: 'models/dafeng_2/dafeng_2.model3.json',
-      thumbnail: 'models/dafeng_2/thumbnail.png'
+      thumbnail: 'models/dafeng_2/textures/texture_00.png'
     },
     {
       name: '吹雪3',
       path: 'models/chuixue_3/chuixue_3.model3.json',
-      thumbnail: 'models/chuixue_3/thumbnail.png'
+      thumbnail: 'models/chuixue_3/textures/texture_00.png'
     },
     {
       name: '标枪3',
       path: 'models/biaoqiang_3/biaoqiang_3.model3.json',
-      thumbnail: 'models/biaoqiang_3/thumbnail.png'
+      thumbnail: 'models/biaoqiang_3/textures/texture_00.png'
     },
     {
       name: '标枪',
       path: 'models/biaoqiang/biaoqiang.model3.json',
-      thumbnail: 'models/biaoqiang/thumbnail.png'
+      thumbnail: 'models/biaoqiang/textures/texture_00.png'
     },
     {
       name: '贝尔法斯特2',
       path: 'models/beierfasite_2/beierfasite_2.model3.json',
-      thumbnail: 'models/beierfasite_2/thumbnail.png'
+      thumbnail: 'models/beierfasite_2/textures/texture_00.png'
     },
     {
       name: '半人马2',
       path: 'models/banrenma_2/banrenma_2.model3.json',
-      thumbnail: 'models/banrenma_2/thumbnail.png'
+      thumbnail: 'models/banrenma_2/textures/texture_00.png'
     },
     {
       name: '埃米尔贝尔丁2',
       path: 'models/aimierbeierding_2/aimierbeierding_2.model3.json',
-      thumbnail: 'models/aimierbeierding_2/thumbnail.png'
+      thumbnail: 'models/aimierbeierding_2/textures/texture_00.png'
     }
   ]);
   
